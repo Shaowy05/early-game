528fa9663d5b2befe1e62b3eeca9bfd07b4f783d
Shaowy05 <jamesshao05@hotmail.com>
2024-01-13 13:24:44 +0000

    Started implementing profile page

---
+++ app/profile/page.tsx
@@ -0,0 +1,13 @@
+import { useContext } from 'react';
+
+export default function Profile() {
+    // const user = useContext();
+
+    return(
+        <>
+            <div>
+                <p></p>
+            </div>
+        </>
+    )
+}
